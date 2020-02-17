91ac13faec50d2c0f1ce023f3713aa8a9d4e164c
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-02-17 11:39:40 +0700

    add input variable

---
+++ 1.multiples-of-3-and-5/input.h
@@ -0,0 +1,13 @@
+#ifndef MULTIPLES_OF_3_AND_5_H_
+#define MULTIPLES_OF_3_AND_5_H_
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+// The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+int first_number = 3;
+int second_number = 5;
+int ceiling_number = 1000;
+
+#endif // MULTIPLES_OF_3_AND_5_H_
