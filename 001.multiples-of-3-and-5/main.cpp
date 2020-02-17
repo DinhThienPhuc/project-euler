07da0886d5ad0074a4eccfb37adeab94ddf77b45
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-02-17 11:57:08 +0700

    refactor main.cpp in first problem

--- 001.multiples-of-3-and-5/main.cpp
+++ 001.multiples-of-3-and-5/main.cpp
@@ -2,16 +2,24 @@
 #include "input.h"
 using namespace std;
 
-int main()
+int getSumDivisibleBy(int num, int ceiling_num)
 {
     int sum = 0;
-    for (int i = 0; i < ceiling_number; i++)
+    for (int i = 0; i < ceiling_num; i++)
     {
-        if (i % first_number == 0 || i % second_number == 0)
+        if (i % num == 0)
         {
             sum += i;
         }
     }
+    return sum;
+}
+
+int main()
+{
+    int sum = getSumDivisibleBy(first_number, ceiling_number) +
+              getSumDivisibleBy(second_number, ceiling_number) -
+              getSumDivisibleBy(first_number * second_number, ceiling_number);
     cout << sum;
     return 0;
 }
