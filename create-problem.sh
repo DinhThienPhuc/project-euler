7be1b22577ad792a7aebc25292d05268078091d6
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-02-20 09:13:48 +0700

    add default format for created problem folder

--- create-problem.sh
+++ create-problem.sh
@@ -4,3 +4,11 @@ mkdir $1
 touch $1/input.h
 touch $1/main.cpp
 touch $1/output
+
+echo "#ifndef _" >> $1/input.h
+echo "#define _ " >> $1/input.h
+echo "#endif // _" >> $1/input.h
+
+echo "#include <iostream>" >> $1/main.cpp
+echo "#include \"input.h\"" >> $1/main.cpp
+echo "using namespace std;" >> $1/main.cpp
