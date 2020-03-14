e1c6ee7373715883d4216c0e5908a784951adb74
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-03-14 11:37:42 +0700

    solving problem 3

--- functions.cpp
+++ functions.cpp
@@ -1,4 +1,23 @@
-bool isPrimeFactor(int number)
+#include <stdio.h>
+#include <math.h>
+
+int isPrimeFactor(int number)
 {
-    
+    if (number == 2)
+    {
+        return 1;
+    }
+    int count = 0;
+    for (int i = 3; i < sqrt(number); i += 2)
+    {
+        if (number % i == 0)
+        {
+            count++;
+        }
+    }
+    if (count > 1)
+    {
+        return 0;
+    }
+    return 1;
 }
