1c9a13679324128eaadb4f24119211e1ecc104c5
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-10-13 10:59:34 +0700

    Mod: update create-problem.sh file for create solution and answer files

--- create-problem.sh
+++ create-problem.sh
@@ -2,13 +2,19 @@
 
 mkdir $1
 touch $1/input.h
-touch $1/main.cpp
-touch $1/output
+touch $1/answer.cpp
+touch $1/answer
+touch $1/solution.cpp
+touch $1/solution
 
-echo "#ifndef _" >> $1/input.h
-echo "#define _ " >> $1/input.h
-echo "#endif // _" >> $1/input.h
+echo "#ifndef _" >>$1/input.h
+echo "#define _ " >>$1/input.h
+echo "#endif // _" >>$1/input.h
 
-echo "#include <iostream>" >> $1/main.cpp
-echo "#include \"input.h\"" >> $1/main.cpp
-echo "using namespace std;" >> $1/main.cpp
+echo "#include <iostream>" >>$1/answer.cpp
+echo "#include \"input.h\"" >>$1/answer.cpp
+echo "using namespace std;" >>$1/answer.cpp
+
+echo "#include <iostream>" >>$1/solution.cpp
+echo "#include \"input.h\"" >>$1/solution.cpp
+echo "using namespace std;" >>$1/solution.cpp
