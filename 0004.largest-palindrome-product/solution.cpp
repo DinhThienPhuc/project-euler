c64116306482aa2b6d806a7d189174aa975fd801
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-10-14 11:13:25 +0700

    Solve: problem 0004

---
+++ 0004.largest-palindrome-product/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+#include "input.h"
+#include "../functions.cpp"
+using namespace std;
+
+int main()
+{
+    int largestPalindrome = 0;
+    int base = 10;
+    int upperBound = pow(base, NUMBER_LETTERS) - 1;
+    int lowerBound = pow(base, NUMBER_LETTERS - 1);
+
+    for (int i = upperBound; i >= lowerBound; i--)
+    {
+        for (int j = upperBound; j >= lowerBound; j--)
+        {
+            int multiResult = i * j;
+            if (isPalindrome(multiResult) && largestPalindrome < multiResult)
+            {
+                largestPalindrome = multiResult;
+            }
+        }
+    }
+    cout << largestPalindrome << endl;
+    return 0;
+}
