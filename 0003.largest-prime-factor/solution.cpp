326d212f83b5136233fd2d891e7874089e8475d7
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-10-14 09:34:40 +0700

    Add: solve problem 0003

---
+++ 0003.largest-prime-factor/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+#include "input.h"
+#include "../functions.cpp"
+using namespace std;
+
+int main()
+{
+    if (isPrime(NUMBER))
+    {
+        return NUMBER;
+    }
+    int maxPrimeFactor = 0;
+    int sqrtOfNumber = sqrt(NUMBER);
+    for (int i = sqrtOfNumber; i >= 1; i--)
+    {
+        if (NUMBER % i == 0 && isPrime(i))
+        {
+            maxPrimeFactor = i;
+            break;
+        }
+    }
+    cout << maxPrimeFactor << "\n";
+    return 0;
+}
