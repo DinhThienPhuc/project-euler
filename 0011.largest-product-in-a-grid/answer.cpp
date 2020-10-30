7a4bbfde9f64fc2e1bd36d954081d9610c231d78
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-10-30 10:28:18 +0700

    Solve: problem-0011

---
+++ 0011.largest-product-in-a-grid/answer.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "input.h"
+using namespace std;
+int main() { return 0;}
---
+++ 0011.largest-product-in-a-grid/solution.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "input.h"
+#include "../functions.cpp"
+using namespace std;
+
+int main()
+{
+    long long result = 1;
+
+    for (int r = 0; r < GRID_SIZE; r++)
+    {
+        for (int c = 0; c < GRID_SIZE; c++)
+        {
+            int adjacentInRow[ADJACENT_NUMBERS_RANGE];
+            int adjacentInCol[ADJACENT_NUMBERS_RANGE];
+            int adjacentInDownDiagon[ADJACENT_NUMBERS_RANGE];
+            int adjacentInUpDiagon[ADJACENT_NUMBERS_RANGE];
+
+            // Adjacent in row
+            for (int i = 0; i < ADJACENT_NUMBERS_RANGE; i++)
+            {
+                if (c <= GRID_SIZE - ADJACENT_NUMBERS_RANGE)
+                {
+                    adjacentInRow[i] = GRID[r][c + i];
+                }
+                if (r <= GRID_SIZE - ADJACENT_NUMBERS_RANGE)
+                {
+                    adjacentInCol[i] = GRID[r + i][c];
+                }
+                if (r <= GRID_SIZE - ADJACENT_NUMBERS_RANGE && c <= GRID_SIZE - ADJACENT_NUMBERS_RANGE)
+                {
+                    adjacentInDownDiagon[i] = GRID[r + i][c + i];
+                }
+                if (r >= ADJACENT_NUMBERS_RANGE - 1 && c <= GRID_SIZE - ADJACENT_NUMBERS_RANGE)
+                {
+                    adjacentInDownDiagon[i] = GRID[r - i][c + i];
+                }
+            }
+            long long products[4] = {
+                getMultipleOfSet(adjacentInRow, ADJACENT_NUMBERS_RANGE),
+                getMultipleOfSet(adjacentInCol, ADJACENT_NUMBERS_RANGE),
+                getMultipleOfSet(adjacentInDownDiagon, ADJACENT_NUMBERS_RANGE),
+                getMultipleOfSet(adjacentInUpDiagon, ADJACENT_NUMBERS_RANGE)};
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (products[i] > result)
+                {
+                    result = products[i];
+                }
+            }
+        }
+    };
+
+    cout << result << endl;
+
+    return 0;
+}
