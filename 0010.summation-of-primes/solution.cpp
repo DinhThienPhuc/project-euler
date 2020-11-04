162f7ebad6f11bfa47a4d0561b3d9a01e05624af
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-11-04 11:45:29 +0700

    Solve: problem-0010

---
+++ 0010.summation-of-primes/solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "input.h"
+#include "../functions.cpp"
+using namespace std;
+
+int main()
+{
+    long sum = 2 + 3;
+    long lowerBound = 1;
+    long upperBound = floor(LIMIT - 1) / 6;
+
+    for (long k = lowerBound; k <= upperBound; k++)
+    {
+        long num1 = 6 * k - 1;
+        long num2 = 6 * k + 1;
+        if (isPrime(num1))
+        {
+            sum += num1;
+        }
+        if (isPrime(num2))
+        {
+            sum += num2;
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
