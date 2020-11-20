101ea3b589c5c58b996e815f347fbf709352e357
dinhthienphuc <thienphuc.bka1412@gmail.com>
2020-11-20 14:19:52 +0700

    Mod: save current working

--- problems/0021/all.test.js
+++ problems/0021/all.test.js
@@ -20,7 +20,19 @@ describe("[AMICABLE_NUMBERS] - Test solution:", () => {
 })
 
 describe("[AMICABLE_NUMBERS] - Test answer:", () => {
-    test("----", async () => {
-        expect(answer()).toBeNull()
+    test("Validate invalid input", async () => {
+        expect(answer("")).toEqual(0)
+    })
+    test("Input: 1000 - Get: 504", async () => {
+        expect(answer(1000)).toEqual(504)
+    })
+    test("Input: 2000 - Get: 2898", async () => {
+        expect(answer(2000)).toEqual(2898)
+    })
+    test("Input: 5000 - Get: 8442", async () => {
+        expect(answer(5000)).toEqual(8442)
+    })
+    test("Input: 10000 - Get: 31626", async () => {
+        expect(answer(10000)).toEqual(31626)
     })
 })
